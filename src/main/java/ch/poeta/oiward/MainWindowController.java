b381d3623f47b6aeea6eedcf6b55fffa5e9f7194
Kalel <kalel.jong_poeta@i-azo.ch>
2022-04-28 11:30:21 +0200

    Switch scene function coded

--- src/main/java/ch/poeta/oiward/MainWindowController.java
+++ src/main/java/ch/poeta/oiward/MainWindowController.java
@@ -1,15 +1,39 @@
 package ch.poeta.oiward;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainWindowController
 {
 	public Canvas cvCanvas;
 	public Pane pnPane;
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 
-	public void onClick(MouseEvent mouseEvent)
+	public void onClick(MouseEvent mouseEvent) throws IOException
 	{
+		switchToMenuWindow(mouseEvent);
 	}
+
+
+
+	public void switchToMenuWindow(MouseEvent event) throws IOException
+	{
+		root = FXMLLoader.load(getClass().getResource("menuWindow.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+
 }
