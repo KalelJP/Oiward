cbd423d2583eb2dd4b1c9980085dc33b85ab12ed
Kalel <kalel.jong_poeta@i-azo.ch>
2022-04-28 16:26:01 +0200

    Square spawning finished, added size slider

--- src/main/java/ch/poeta/oiward/MainWindowController.java
+++ src/main/java/ch/poeta/oiward/MainWindowController.java
@@ -1,12 +1,8 @@
 package ch.poeta.oiward;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -14,30 +10,24 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MainWindowController
 {
 	public Pane pnPane;
 	public Label lblSize;
 	public ColorPicker cpForm;
-	public TextField txtSize;
 	public Group gpShapes;
 	public RowConstraints menuRowTwo;
 	public RowConstraints menuRowOne;
 	public GridPane gdpMenu;
 	public HBox hbxOptions;
+	public Slider sldSize;
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-
-	private boolean isSquare = false;
 
 	public ComboBox cbxGeoForm;
 	
@@ -45,7 +35,7 @@ public class MainWindowController
 	public void initialize()
 	{
 		cbxGeoForm.getItems().add("Circle");
-		cbxGeoForm.getItems().add("Rectangle");
+		cbxGeoForm.getItems().add("Square");
 		cbxGeoForm.getItems().add("Ellipse");
 	}
 
@@ -63,24 +53,25 @@ public class MainWindowController
 
 			}
 		}
+		else if (mouseEvent.getButton() == MouseButton.MIDDLE)
+		{
+			gpShapes.getChildren().clear();
+		}
 		else{
 			try
 			{
 				switch (cbxGeoForm.getValue().toString())
 				{
 					case "Circle":
-						Circle circle = new Circle();
-						circle.setCenterX(mouseEvent.getX());
-						circle.setCenterY(mouseEvent.getY());
-						circle.setRadius(Integer.parseInt(txtSize.getText()));
-						circle.setFill(cpForm.getValue());
+						Circle circle = new Circle(mouseEvent.getX(), mouseEvent.getY(), sldSize.getValue(),
+								cpForm.getValue());
+						gpShapes.getChildren().add(circle);
 						break;
-					case "Rectangle":
-
-						lblSize.setText("Height");
-						txtSize.setPrefWidth(107);
-						Label lblWidth = new Label("Width");
-						hbxOptions.getChildren().add(lblWidth);
+					case "Square":
+						Rectangle rect = new Rectangle(mouseEvent.getX(), mouseEvent.getY(), sldSize.getValue(),
+								sldSize.getValue());
+						rect.setFill(cpForm.getValue());
+						gpShapes.getChildren().add(rect);
 						break;
 					case "Ellipse":
 						//shape = new Ellipse();
