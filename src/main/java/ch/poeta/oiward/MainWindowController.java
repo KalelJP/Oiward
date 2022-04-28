397b456f59c43416484d088b0270248f5e12b016
Kalel <kalel.jong_poeta@i-azo.ch>
2022-04-28 10:16:14 +0200

    Delete GeoForm class. Added click event to main window

--- src/main/java/ch/poeta/oiward/MainWindowController.java
+++ src/main/java/ch/poeta/oiward/MainWindowController.java
@@ -1,10 +1,15 @@
 package ch.poeta.oiward;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class MainWindowController
 {
 	public Canvas cvCanvas;
 	public Pane pnPane;
+
+	public void onClick(MouseEvent mouseEvent)
+	{
+	}
 }
--- src/main/java/ch/poeta/oiward/MenuController.java
+++ src/main/java/ch/poeta/oiward/MenuController.java
@@ -1,5 +1,14 @@
 package ch.poeta.oiward;
 
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class MenuController
 {
+	public Label lblSize;
+	public ComboBox cbxGeoForm;
+	public ColorPicker cpForm;
+
+
 }
